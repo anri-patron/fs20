c72d03e749a9356a659d62fb5519d0856bc4da30
Anri Patron <nayamigoto@gmail.com>
2020-07-10 14:34:49 +0300

    1.6

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom';
 
+const Button = ({handleClick, text}) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
 const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  // tapahtumankäsittelijät napeille
+  const handleGood = () => {
+    setGood(good + 1)
+  }
+  const handleNeutral = () => {
+    setNeutral(neutral + 1)
+  }
+  const handleBad = () => {
+    setBad(bad + 1)
+  }
 
   return (
     <div>
-    <p>hei</p>
+      <div>
+      <h1>Give feedback</h1>
+      <Button handleClick={handleGood} text='good'></Button>
+      <Button handleClick={handleNeutral} text='neutral'></Button>
+      <Button handleClick={handleBad} text='bad'></Button>
+      </div>
+
+      <div>
+      <h2>Statistics</h2>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      </div>
     </div>
   )
 }
